dce180d2342e029316916b1209dee12e1a1ccf4a
cmj <chmin82@gmail.com>
2021-10-03 01:04:04 +0900

    String Class Test

--- src/main/java/main/java/StringClass.java
+++ src/main/java/main/java/StringClass.java
@@ -3,6 +3,7 @@ package main.java;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 
 public class StringClass {
@@ -19,9 +20,19 @@ public class StringClass {
 	/*
 	 * String 에서 괄호를 제거하는 메소드 
 	 */
-	public String removeParenthesesFromString(String string) {
-		String result = "";
-		// TODO Auto-generated method stub
+	public String removeParenthesesFromString(String str) throws StringIndexOutOfBoundsException{
+		String result = str.substring(str.indexOf("(")+1, str.indexOf(")"));
+		return result;
+	}
+
+	public char getCharFromString(String str, int i) {
+		char result = '\0';
+		try {
+			result = str.charAt(i);
+			
+		} catch(StringIndexOutOfBoundsException e) {
+			throw new StringIndexOutOfBoundsException("Index: "+ i +", Size: " + str.length());
+		}
 		return result;
 	}
 }
--- src/test/java/test/java/StringClassTest.java
+++ src/test/java/test/java/StringClassTest.java
@@ -8,24 +8,10 @@ import org.junit.jupiter.api.DisplayName;
 import main.java.StringClass;
 
 public class StringClassTest {
-	
+
 	/*
-	 * assertj 테스트
-	 */
-	@Test
-	public void aFewSimpleAssertions() {
-		assertThat("Hello, world! Nice to meet you.") // 주어진 "Hello, world! Nice to meet you."라는 문자열은
-		.isNotEmpty() // 비어있지 않고
-		.contains("Nice") // "Nice"를 포함하고
-		.contains("world") // "world"도 포함하고
-		.doesNotContain("ZZZ") // "ZZZ"는 포함하지 않으며
-		.startsWith("Hell") // "Hell"로 시작하고
-		.endsWith("u.") // "u."로 끝나며
-		.isEqualTo("Hello, world! Nice to meet you."); // "Hello, world! Nice to meet you."과 일치합니다.
-	}
-	
-	/*
-	 * 요구사항1
+	 * 요구사항1 1. "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다. 2. "1"을 ,로
+	 * split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
 	 */
 	@Test
 	public void separateStringToArrayAssertions() {
@@ -33,24 +19,35 @@ public class StringClassTest {
 		assertThat(sc.separateStringToArray("1,2")).hasSize(2).containsExactly("1", "2");
 		assertThat(sc.separateStringToArray("1,")).hasSize(1).containsExactly("1");
 	}
-	
+
 	/*
-	 * 요구사항2
+	 * 요구사항2 1. "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 확용해 ()을 제거하고 "1,2"를 반환하도록
+	 * 구현한다.
 	 */
 	@Test
 	public void removeParenthesesFromStringAssertions() {
 		StringClass sc = new StringClass();
-		assertThat(sc.removeParenthesesFromString("(1,2)")).contains("1,2");
+		assertThat(sc.removeParenthesesFromString("(1,2)")).isEqualTo("1,2");
 	}
-	
+
 	/*
-	 * 요구사항3
+	 * 요구사항3 1. "abc"값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를
+	 * 구현한다. 2. String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져올 때 위치 값을 벗어나면
+	 * StringIndexOutOfBoundsException이 발생하는 부분에 대한 학습 테스트를 구현한다. 3.
+	 * JUnit의 @DisplayName을 활용해 테스트 메소드의 의도를 드러낸다.
 	 */
 	@Test
-	@DisplayName("DisplayName 은 어디에서 쓰나요?")
+	@DisplayName("특정위치를 입력하면 해당 위치의 문자를 리턴받는 메소드 검증")
 	public void getCharFromStringAssertions() {
 		StringClass sc = new StringClass();
-		assertThat(sc.removeParenthesesFromString("(1,2)")).contains("1,2");
+		assertThat(sc.getCharFromString("abc", 0)).isEqualTo('a');
+		assertThat(sc.getCharFromString("abc", 1)).isEqualTo('b');
+		assertThat(sc.getCharFromString("abc", 2)).isEqualTo('c');
+//		assertThat(sc.getCharFromString("abc", 3)).isInstanceOf(IndexOutOfBoundsException.class).hasMessageContaining("Index:2, Size: 2");
+//		assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> {
+//			throw new IndexOutOfBoundsException("Index: 3, Size: 3");
+//		}).withMessageMatching("Index: \\d+, Size: \\d+");
+		assertThatThrownBy(() -> sc.getCharFromString("abc", 3)).isInstanceOf(IndexOutOfBoundsException.class).hasMessageContaining("Index: 3, Size: 3");
 	}
 
 }
